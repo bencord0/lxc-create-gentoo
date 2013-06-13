d863853c1359828e2f767921aa694640974fa81e
Ben Cordero (bencord0) <bmc@linux.com>
2013-06-13 12:14:14 +0000

    License under GPLv3

--- fixup-stage3-for-lxc.sh
+++ fixup-stage3-for-lxc.sh
@@ -1,3 +1,22 @@
+#
+# Copyright 2013 Ben Cordero
+#
+# This file is part of lxc-create-gentoo.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 ln -vs /etc/init.d/sshd etc/runlevels/default/sshd
 ln -vs net.lo etc/init.d/net.eth0
 ln -vs /etc/init.d/net.eth0 etc/runlevels/boot/net.eth0
